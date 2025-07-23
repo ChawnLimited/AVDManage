44a7dc9f04c3d1fa0c95a78681d26f0e43cb765d
ChawnLimited <dg@chawn.com>
2025-07-23 03:37:00 +0100

    Add files via upload

--- AVD-Turbo.ps1
+++ AVD-Turbo.ps1
@@ -1,12 +1,11 @@
 # Chawn Limited 2025
 # AVD-Turbo.ps1
-# Version 1.0
-# Rename and Join a VM (created from a snapshot) to Active Directory, installs AVD Agents - way quicker than creating VMs from images
+# Version 1.1
+# Rename the VM, optionally Join VM (created from a specialized image) to Active Directory, and optionally install AVD Agents
 
 #### Parameters
 
   param(
-	[String]$VMName= "",			# New VMName
 	[String]$ADDomain= "",			# Set the domain name in FQDN format
 	[String]$OU= "",			# Set the Organisational Unit for the VM
 	[String]$ADAdmin= "",			# Set the domain join user account
@@ -20,7 +19,8 @@
 
 #### End of Parameters
 
-$Logfile = "TurboAVD.log"
+$ProgressPreference ="SilentlyContinue"
+$Logfile = "AVD-Turbo.log"
 
 Function LogWrite
 {
@@ -31,23 +31,168 @@ Function LogWrite
 
 LogWrite "Starting up"
 
+	$d1=get-Date
+	LogWrite ("Renaming Computer")
+	do {
+	start-sleep -seconds 1
+	} until ((Get-ChildItem -Path C:\WindowsAzure\config -Filter *.xml).count -eq 1)
+
+try {
+
+	$d2=get-Date
+	$dur=$d2-$d1
+	LogWrite ("Duration: " + $dur.Hours + " hours " + $dur.Minutes + " mins " + $dur.Seconds + " secs")
+	$xml=Get-ChildItem -Path C:\WindowsAzure\config -Filter *.xml | sort-object -Property CreationTime -descending
+	
+	$xmlfile = New-Object xml
+	$xmlfile.Load($xml[0].fullname)
+	
+	$xpath="/RDConfig/Instances/Instance"
+	$vmname=(Select-Xml -Path $xml.fullname -XPath $xpath | Select-Object -ExpandProperty Node).id
+	$vmname=$vmname.Substring(1)
+	
 	if((gwmi win32_computersystem).partofdomain -eq 0) {
 	LogWrite ("Renaming Computer to " + $VMName)
 	Rename-Computer -NewName $VMName -Force | Out-File -FilePath $Logfile -Append
-	$ADDomainCred = New-Object pscredential -ArgumentList ([pscustomobject]@{
-	UserName = $ADAdmin
-	Password = (ConvertTo-SecureString -String $ADAdminPW -AsPlainText -Force)[0]})
+	}
+    }
+Catch {
+LogWrite ($_.Exception.Message)
+exit 1
+}
 
-	LogWrite ("Join Domain " + $ADDomain)
-	Add-Computer -DomainName $ADDomain -OUPath $ou -Credential $ADDomainCred -Options JoinWithNewName,AccountCreate -Force -PassThru -Verbose | Out-File -FilePath $Logfile -Append
+
+If ($ADDomain) {
+		if((gwmi win32_computersystem).partofdomain -eq 0) {
+
+try {
+		$ADDomainCred = New-Object pscredential -ArgumentList ([pscustomobject]@{
+		UserName = $ADAdmin
+		Password = (ConvertTo-SecureString -String $ADAdminPW -AsPlainText -Force)[0]})
+
+		LogWrite ("Join Domain " + $ADDomain)
+		Add-Computer -DomainName $ADDomain -OUPath $ou -Credential $ADDomainCred -Options JoinWithNewName,AccountCreate -Force -PassThru -Verbose | Out-File -FilePath $Logfile -Append
+		LogWrite ("Ignore the Computername above. Add-Computer always reports the original name, not the new name.")
+    }
+catch{
+LogWrite ($_.Exception.Message)
+exit 2
+}
+
+		}
+	      }
+
+
+If ($HostPool) {
+
+	%{
+		if (get-item -path "C:\Program Files\Microsoft RDInfra" -ErrorAction SilentlyContinue)
+		{LogWrite ("Remote Desktop Agents are already installed. Exit")
+        exit 3}
 	}
 
-LogWrite "Install RDS Agents "
-	$Procargs="-ExecutionPolicy ByPass -NoProfile -File AVDJoin.ps1 " + $HostPool + " " + $RG + " " + $ClientID  + " " + $ClientSecret + " " + $TenantID
-	# Start-Process -FilePath "powershell.exe" -ArgumentList $ProcArgs -Wait
-LogWrite "Install RDS Agents completed. View JoinWVD.log for details and troubleshooting"
+# check the device is domain joined
+	%{
+	if ((gwmi win32_computersystem).partofdomain -eq $false) {logwrite('Device is not AD Domain joined. Exit.')
+	exit 4}
+	else {logwrite('Device is AD Domain joined.')}
+	}
 
-LogWrite "Schedule a restart and exit"
-Start-Process -FilePath shutdown.exe -ArgumentList "-r -t 10"
-exit 0
+
+# get the DNS hostname of the VM
+	$hostname=$vmname + "." + $ADDomain
+	logwrite('Hostname:' + $hostname)
+	logwrite('Hostpool:' + $hostpool)
+	logwrite('ClientID:' + $ClientID)
+
+### Create the AVD Agent PSCredential
+	$AVDCred = New-Object pscredential -ArgumentList ([pscustomobject]@{
+	    UserName = $ClientId
+	    Password = (ConvertTo-SecureString -String $ClientSecret -AsPlainText -Force)[0]})
+	logwrite('Created PSCreds for Azure')
+
+# Check AZ Modules are present
+	%{
+		try {import-module Az.Accounts, Az.DesktopVirtualization
+		if (Get-Module -name Az.Accounts) {Logwrite('Az.Accounts is available')}
+		else {logwrite('Az.Accounts is not available. Exit.')
+			exit 5}
+		if (Get-Module -name Az.DesktopVirtualization) {Logwrite('Az.DesktopVirtualization is available')}
+		else {logwrite('Az.DesktopVirtualization is not available. Exit.')
+			exit 5}
+		}
+		catch {logwrite('Error importing Az Modules')
+			exit 5}
+	}
+
+Disable-AzContextAutosave -Scope Process
+
+# Logon to Azure
+	%{
+		try {Connect-AzAccount -ServicePrincipal -TenantId $TenantId -Credential $AVDCred
+		if ((Get-AZAccessToken -ErrorAction SilentlyContinue).count -ne 0) {logwrite('Connected to Azure')}
+		else {logwrite('Not connected to Azure. Exit.')
+		exit 6}
+		}
+		catch{logwrite('Error connecting to Azure')
+			exit 6}
+	}
+
+# check if the VM exists in the hostpool, if so remove it
+
+	%{
+	if (Get-AzWvdSessionHost -HostPoolName $hostpool -ResourceGroupName $RG -Name $hostname -ErrorAction SilentlyContinue) 
+
+		{Remove-AzWvdSessionHost -ResourceGroupName $RG -HostPoolName $HostPool -Name $hostname -ErrorAction stop
+		logwrite ($hostname + ' exists in the ' + $hostpool + ' host pool. Will remove so the VM may join again.')}
+	}
+
+
+# check if a valid Token exists to join the hostpool, if not generate one
+
+	$now=(get-date).addhours(2)
+	%{
+		if ($now -gt (Get-AzWvdRegistrationInfo -ResourceGroupName $RG -HostPoolName $HostPool).ExpirationTime)
+			{logwrite ('Generate new WVD Token to join WVD Hostpool: ' + $HostPool)
+			$WVDToken=(New-AzWvdRegistrationInfo -ResourceGroupName $RG -HostPoolName $HostPool -ExpirationTime $((get-date).ToUniversalTime().AddHours(25).ToString('yyyy-MM-ddTHH:mm:ss.fffffffZ'))).Token}
+		Else {logwrite ('WVDToken exists for Hostpool: ' + $HostPool)
+		$WVDToken=(Get-AzWvdRegistrationInfo -ResourceGroupName $RG -HostPoolName $HostPool).Token}
+	}
+
+
+# deploy the RDAgent and RDBootloader
+
+	%{
+	if ($WVDToken)
+		{
+		logwrite ('WVD Token to join WVD Hostpool: ' + $WVDToken)
+
+		### Install RDAgent
+		logwrite('Install Remote Desktop Services Infrastructure Agent')
+		$URI="https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrmXv"
+		Invoke-WebRequest -Uri $URI -OutFile RDAgent.msi -UseBasicParsing
+		Start-Process msiexec.exe -Wait -ArgumentList "/I RDAgent.msi REGISTRATIONTOKEN=$WVDToken /qb /L*V RDAgent.log"
+		
+		### Install RDBroker
+		logwrite ('Install Remote Desktop Agent Boot Loader')
+		$URI="https://query.prod.cms.rt.microsoft.com/cms/api/am/binary/RWrxrH"
+		Invoke-WebRequest -Uri $URI -OutFile RDBoot.msi -UseBasicParsing
+		Start-Process msiexec.exe -Wait -ArgumentList "/I RDBoot.msi /qb  /L*V RDBoot.log"
+		LogWrite "Install RDS Agents completed."
+		}
+		Else {logwrite ('Could not retrieve a WVD Host Token for HostPool:' + $HostPool + '. Skip join WVD Hostpool')}
+	}
+
+    # Logout of Azure
+	    Disconnect-AzAccount
+	    logwrite ('Disconnected from Azure')
+
+	     }
+
+
+
+	LogWrite "Schedule a restart and exit"
+	Start-Process -FilePath "shutdown.exe" -ArgumentList "-r -t 10"
+	exit 0
+
 

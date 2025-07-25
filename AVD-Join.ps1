8b735195c514df6561bcae1a3f4311c6082fa104
ChawnLimited <dg@chawn.com>
2025-07-25 01:22:06 +0100

    Add files via upload

--- AVD-Join.ps1
+++ AVD-Join.ps1
@@ -78,9 +78,9 @@ else {logwrite('Device is AD Domain joined.')}
 %{
 		try {
 
-		if (Get-PackageProvider -Name Nuget) {Logwrite('Nuget is available')}
+		if (Get-PackageProvider -Name Nuget -ListAvailable) {Logwrite('Nuget is available')}
 		else {logwrite('Nuget is not available. Will try and install.'); UpdateNuget;
-    		if (Get-PackageProvider -Name Nuget) {Logwrite('Nuget is available')}
+    		if (Get-PackageProvider -Name Nuget -ListAvailable) {Logwrite('Nuget is available')}
 	    	else {logwrite('Nuget is not available. Exit.'); exit 3}
              }
 

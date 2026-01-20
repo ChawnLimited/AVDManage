17948d7fc9c32bdd9f81191f8829fc9175899798
ChawnLimited <dg@chawn.com>
2026-01-20 23:35:55 +0000

    Add files via upload

--- AVD-Prep.ps1
+++ AVD-Prep.ps1
@@ -1,10 +1,11 @@
-# Chawn Limited 2025
+# Chawn Limited 2026
 # AVD-Prep.ps1
-# Version 3.0
+# Version 4.0
+# Remove RDSIA and RDSBL if instaleld
 # Download / Update Microsoft Remote Desktop Infrastructure and Boot Agents
 # Install Agents silently with no Registration Token
 # Neutralise the Agents before running sysprep
-# This allows AVDTurbo and AVDJoin to perform faster deployments
+# This allows AVDTurbo to perform faster deployments
 
 $ProgressPreference ="SilentlyContinue"
 $Logfile = "AVD-Prep.log"
@@ -15,6 +16,16 @@ Function LogWrite
 	$d1=get-Date
    Add-content $Logfile -value ($d1.tostring() + " : " + $logstring)
 }
+
+# Remove existing agents if present
+%{
+	try {
+		if (get-package -name 'Remote Desktop Agent Boot Loader') {Uninstall-Package -Name 'Remote Desktop Agent Boot Loader' -AllVersions -Force; LogWrite ("Uninstalled Remote Desktop Agent Boot Loader")}
+		if (get-package -name 'Remote Desktop Services Infrastructure Agent') {Uninstall-Package -Name 'Remote Desktop Services Infrastructure Agent' -AllVersions -Force; ; LogWrite ("Remote Desktop Services Infrastructure Agent")}
+	}
+	catch {}
+}
+
 
 
 # Start the RDAGent downloads
@@ -36,7 +47,7 @@ LogWrite ("Install & Configure RDAgents.")
 	    if (get-item -path C:\Source\RDagent.msi){Start-Process msiexec.exe -Wait -ArgumentList "/I C:\Source\RDAgent.msi REGISTRATIONTOKEN=[INVALIDTOKEN] /qb /L*V RDAgent.log"}
 	    if (get-item -path C:\Source\RDBoot.msi){Start-Process msiexec.exe -Wait -ArgumentList "/I C:\Source\RDBoot.msi /qb  /L*V RDBoot.log"}		
 			$i=0
-			do {start-sleep -Seconds 2;$i++;} until((($RDSIA=(get-package -name "Remote Desktop Services Infrastructure Agent" -ErrorAction SilentlyContinue).Status -eq 'Installed')) -and (($RDABL=(get-package -name "Remote Desktop Agent Boot Loader" -ErrorAction SilentlyContinue).Status -eq 'Installed')) -or $i -eq 50)
+			do {start-sleep -Seconds 2;$i++;} until((($RDSIA=(get-package -name "Remote Desktop Services Infrastructure Agent" -ErrorAction SilentlyContinue).Status -eq 'Installed')) -and (($RDSABL=(get-package -name "Remote Desktop Agent Boot Loader" -ErrorAction SilentlyContinue).Status -eq 'Installed')) -or $i -eq 50)
 				if (($RDSIA -eq 'Installed' ) -and ($RDSABL -eq 'Installed'))
 				{logwrite ("Remote Desktop Services Infrastructure Agent and Remote Desktop Agent Boot Loader are installed")}
 				Else {logwrite ("Remote Desktop Services Infrastructure Agent installed: " + $RDSIA + ". Remote Desktop Agent Boot Loader installed: " + $RDSABL)}

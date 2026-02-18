26294f6fb8e7f99b5eaa4c9b38dcafce8946f824
ChawnLimited <dg@chawn.com>
2026-02-18 17:29:20 +0000

    Add files via upload

--- AVD-Turbo5.ps1
+++ AVD-Turbo5.ps1
@@ -272,7 +272,7 @@ CheckEntraID
 	if ($ADDomain){
 		$i=0		
 		do {start-sleep -Seconds 1;$i++;} until ((Test-NetConnection -ComputerName $ADDomain -CommonTCPPort SMB -InformationLevel Quiet) -or $i -eq 15)
-		if ($i -eq 30) {LogWrite ("Cannot connect to " + $ADDomain + ". Abort joining Active directory. Exit"); exit 302} else{LogWrite ($ADDomain + " is available after " + $i + " attempts.")}
+		if ($i -eq 15) {LogWrite ("Cannot connect to " + $ADDomain + ". Abort joining Active directory. Exit"); exit 302} else{LogWrite ($ADDomain + " is available after " + $i + " attempts.")}
 		if($NotDomainJoined) {JoinDomain;CheckDomain;}
 		else{Logwrite ($AZVMName + " is already domain joined.")}
 	}
@@ -284,7 +284,7 @@ CheckEntraID
 	{	
 		$i=0
 		do {start-sleep -Seconds 1;$i++;} until ((Test-NetConnection -ComputerName 169.254.169.254 -CommonTCPPort HTTP -InformationLevel Quiet) -or $i -eq 15)
-		if ($i -eq 30) {LogWrite ("Cannot connect to Entra ID. Abort joining Entra ID. Exit"); exit 406} else{LogWrite ("Azure is available after " + $i + " attempts.")}
+		if ($i -eq 15) {LogWrite ("Cannot connect to Entra ID. Abort joining Entra ID. Exit"); exit 406} else{LogWrite ("Azure is available after " + $i + " attempts.")}
 		if ($IsEntraJoined -eq "NO") {JoinEntraID}
 	}
 }

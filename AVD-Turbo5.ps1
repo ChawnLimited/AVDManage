edfc4d02f4a0a8be3fb95503f8e4cc28aed33cad
ChawnLimited <dg@chawn.com>
2026-02-23 04:31:41 +0000

    Add files via upload

--- AVD-Turbo5.ps1
+++ AVD-Turbo5.ps1
@@ -58,7 +58,7 @@ Function CheckDomain
 			$exp=get-date($exp) -Format yyyy-MM-ddTHH:mm:ss
 			$at.Triggers[0].EndBoundary=$exp
 			#$actions = New-ScheduledTaskAction -Execute PowerShell.exe -Argument "-NoProfile -Command {Do {start-sleep -seconds 45} until ((Start-Process %SystemRoot%\System32\dsregcmd.exe -ArgumentList '/Join /debug' -Wait -RedirectStandardOutput C:\Windows\Temp\dsregcmd.log).ExitCode -eq 0)}"
-			$actions = New-ScheduledTaskAction -Execute PowerShell.exe -Argument "-NoProfile -Command {Do {start-sleep -seconds 45} until ((Start-Process %SystemRoot%\System32\dsregcmd.exe -ArgumentList '/Join /debug > c:\temp\dsreg.log' -Wait ).ExitCode -eq 0)}"
+			$actions = New-ScheduledTaskAction -WorkingDirectory %SystemRoot%\System32 -Execute PowerShell.exe -Argument "-NoProfile -Command {Do {start-sleep -seconds 45} until ((Start-Process %SystemRoot%\System32\dsregcmd.exe -Wait ).ExitCode -eq 0)}"
 			$at.actions=$actions
 			$repetition = New-CimInstance `
 			-Namespace "Root/Microsoft/Windows/TaskScheduler" `

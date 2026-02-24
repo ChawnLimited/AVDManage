0ab6f38dd0f6475be676efa33898a83ca694973b
ChawnLimited <dg@chawn.com>
2026-02-24 22:18:06 +0000

    Add files via upload

--- AVD-Turbo5.ps1
+++ AVD-Turbo5.ps1
@@ -209,7 +209,7 @@ Function RenameComputer
 
 Function JoinDomain
 {
-    try{LogWrite ($ADdomain $AdAdmin $OU $ADAdminPW)
+    try{LogWrite ($ADdomain + " " + $AdAdmin + " " + $OU + " " + $ADAdminPW)
         LogWrite ("Join Domain. Create Credentials.")
 		$ADDomainCred = New-Object pscredential -ArgumentList ([pscustomobject]@{
 		UserName = $ADAdmin

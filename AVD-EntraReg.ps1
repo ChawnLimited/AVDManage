69ed98f3e64528060f9ebe2d2f3c8eb1256e9b26
ChawnLimited <dg@chawn.com>
2026-02-26 18:09:58 +0000

    Add files via upload

--- AVD-EntraReg.ps1
+++ AVD-EntraReg.ps1
@@ -32,7 +32,7 @@ Logwrite("Starting Up")
 %{
 	try {
 		$i=0
-		Do{$i++; $Proc=Start-Process -FilePath dsregcmd.exe -ArgumentList '$(Arg0) $(Arg1) $(Arg2) /Debug' -RedirectStandardOutput AVD-EntraJoin.log -Wait -Passthru; LogWrite("Attempt:" + $i + " - DSRegCmd completed with exit code: " + $Proc.exitcode);CheckEntraID;if($IsEntraJoined -eq "YES"){break};Start-sleep -seconds 90} until ($IsEntraJoined -eq "YES" -or $i -eq 20)
+		Do{if($IsEntraJoined -eq "YES"){LogWrite("Already Entra joined. Exit.");break};$i++; $Proc=Start-Process -FilePath dsregcmd.exe -ArgumentList '$(Arg0) $(Arg1) $(Arg2) /Debug' -RedirectStandardOutput AVD-EntraJoin.log -Wait -Passthru; LogWrite("Attempt:" + $i + " - DSRegCmd completed with exit code: " + $Proc.exitcode);CheckEntraID;if($IsEntraJoined -eq "YES"){break};Start-sleep -seconds 90} until ($IsEntraJoined -eq "YES" -or $i -eq 20)
 
 		if ($IsEntraJoined -eq "NO") {LogWrite("AVDEntraReg has NOT joined Entra after " + $i + " attempts.");exit 9999}
 		else {LogWrite("AVDEntraReg has joined Entra after " + $i + " attempts. Job completed.");exit 0}

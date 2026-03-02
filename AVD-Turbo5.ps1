47e5a1615f8f7e9fc5623b96c97f11648dabe43f
ChawnLimited <dg@chawn.com>
2026-03-02 03:43:44 +0000

    Add files via upload

--- AVD-Turbo5.ps1
+++ AVD-Turbo5.ps1
@@ -462,7 +462,7 @@ CheckToken
 		if ($AVDLogons -eq "False") {
 		$i=0
 		$GETHostUri="https://management.azure.com/subscriptions/$subId/resourceGroups/$RG/providers/Microsoft.DesktopVirtualization/hostPools/$hostPool/sessionHosts/$HostName/?api-version=2024-04-03&force=true/?api-version=2024-04-03"
-		do {start-sleep -seconds 5;$i++;$SessionHost=Invoke-RestMethod -Uri $GETHostURI -Method Get -Headers $Headers -Body $body -ErrorAction SilentlyContinue} until ($SessionHost.count -eq 1 -or $i -eq 20)
+		do {start-sleep -seconds 10;$i++;$ErrorActionPreference="SilentlyContinue"$SessionHost=Invoke-RestMethod -Uri $GETHostURI -Method Get -Headers $Headers -Body $body -ErrorAction SilentlyContinue} until ($SessionHost.count -eq 1 -or $i -eq 20)
 			logwrite ('Disable AVD Logons for: ' + $hostname + ' Hostpool:' + $HostPool)
 			$HostURI = "https://management.azure.com/subscriptions/$subId/resourceGroups/$RG/providers/Microsoft.DesktopVirtualization/hostPools/$hostPool/sessionHosts/$HostName/?api-version=2024-04-03&force=true"
 				$body = @{
@@ -471,10 +471,10 @@ CheckToken
 				}
 			} | ConvertTo-Json -Depth 3
 			Invoke-RestMethod -Uri $HostURI -Method Patch -Headers $Headers -Body $body
-			logwrite ('AVD Logons disabled for: ' + $hostname + ' Hostpool:' + $HostPool)
+			logwrite ('AVD Logons disabled for: ' + $hostname + ' Hostpool:' + $HostPool;$ErrorActionPreference="Continue")
 			}
 	}
-    catch {Logwrite("1001 Failed to Disable AVD logons: " + $_.Exception.Message)}
+    catch {Logwrite("1001 Failed to Disable AVD logons: " + $_.Exception.Message);$ErrorActionPreference="Continue"}
 }
 
 
